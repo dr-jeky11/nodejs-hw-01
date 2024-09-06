090693e2e307c1d7ae47b349bdbdf3abee85616b
dr-jeky11 <tyrsyna@gmail.com>
2024-09-06 23:24:47 +0200

    remove all and last one, rerun every script

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) {
+    console.log('No contacts to remove');
+    return;
+  }
+  contacts.pop();
+  await writeContacts(contacts);
+};
 
 removeLastContact();
